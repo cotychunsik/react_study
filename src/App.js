70f6a30b99c6b92f5614107b323a0c99d0463a6a
cotychunsik <toboldygo@naver.com>
2022-09-08 19:25:08 +0900

    nothing has changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post= '리액트 기초강의';
+  let [title,b] = useState(['무료강의 추천','C는 언제공부함','할거졸라많']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>it's blog</div>
+      </div>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
